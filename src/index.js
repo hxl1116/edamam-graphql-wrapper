aa589796f1d25dc96651460d6e0b51b9c7583dd9
Henry Larson <hxl1116@rit.edu>
2021-04-13 18:22:26 -0400

    Fixed up index.js, imported 'dotenv'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
-
-// Import resolves and type definitions
 const {typeDefs, resolvers} = require('./api/root.api.js');
+require('dotenv').config();
 
-// Define app and server
-const port = process.env.PORT || 8080;
 const app = express();
 const server = new ApolloServer({typeDefs, resolvers})
-
 server.applyMiddleware({app})
 
-// Start the server
-app.listen({port}, () => {
-    console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
 })
